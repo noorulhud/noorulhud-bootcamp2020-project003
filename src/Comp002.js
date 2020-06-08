a1d5eed48b789d43e874153e29aa393577729e63
Noor ul Huda <63110809+noorulhud@users.noreply.github.com>
2020-06-08 16:26:08 -0500

    v1

---
+++ src/Comp002.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HW_Cmp002(props) {
+  return (
+    <div>
+      <h1>This is my component: {props.alpha}</h1>
+      <p>Para of my component 2.</p>
+    </div>
+  );
+}
+
+export default HW_Cmp002;
---
+++ src/myApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ChildCmpn from "./Comp002";
+
+function ParnComp_001() {
+  return (
+    <div>
+      Hello World from Noor
+      <ChildCmpn alpha="Bravo">hw</ChildCmpn>
+    </div>
+  );
+}
+
+export default ParnComp_001;
